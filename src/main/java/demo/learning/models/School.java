a80343f5a5d9bf81738dd9f32b4000277822ef20
Rajshree <rajshreekashyap36@gmail.com>
2024-11-06 10:16:06 +0530

    adding school class

---
+++ src/main/java/demo/learning/models/School.java
@@ -0,0 +1,19 @@
+package demo.learning.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class School {
+
+    private String school_name;
+    private int school_ID;
+
+
+}
