d10df68b70df9e6c00d56584f29b96312a4c976a
Rajshree <rajshreekashyap36@gmail.com>
2024-11-06 10:06:49 +0530

    fisrt commit

---
+++ src/main/java/demo/learning/models/Students.java
@@ -0,0 +1,27 @@
+package demo.learning.models;
+
+public class Students {
+
+    private String name;
+    private int ID;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+
+
+}
---
+++ src/test/java/demo/learning/LearningGitApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
